dbcc064176eb1191bf8666ac04bdd7fb456d6ca9
os-born <ol.serduk@gmail.com>
2021-07-11 18:58:15 +0300

    Task2Done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+  
+const arrOfLiItem = ingredients.map(liItem => {
+const liEl = document.createElement("li");
+liEl.textContent = liItem;
+return liEl;
+}
+)
+
+console.log(arrOfLiItem);
+
+const ulElIngredients = document.querySelector("#ingredients");
+
+ulElIngredients.append(...arrOfLiItem);
