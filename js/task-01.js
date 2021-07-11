9932aa16ab36157a9293832524ff923df63a2257
os-born <ol.serduk@gmail.com>
2021-07-11 14:36:12 +0300

    1Task_7Module_Done

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const categories = document.querySelectorAll('li.item');
+console.log(`В списке ${categories.length} категории`);
+const arr = categories.forEach(category => {
+       
+        console.log('Категория: ', category.firstElementChild.textContent);
+        const quantityOfCategoryList = category.firstElementChild.nextElementSibling.querySelectorAll('li').length;
+        return console.log(`Количество элементов: ${quantityOfCategoryList}`);
+    }
+    
+
+)
