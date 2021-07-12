54d666d58c4b3b29e745ffe8c5d444b9060c7a4d
os-born <ol.serduk@gmail.com>
2021-07-12 20:37:15 +0300

    6Task_7Module_Done_edited

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,16 @@
 
 const inputRef = document.querySelector('#validation-input');
 
-const inputLength = input.getAttribute('data-length');
+const inputLength = inputRef.getAttribute('data-length');
 
 function getColor() {
 
     const inputLengthToNumber = Number(inputLength);
 
-    if (inputLengthToNumber === input.value.length) {
+    if (inputLengthToNumber === inputRef.value.length) {
         inputRef.classList.add('valid');
         inputRef.classList.remove('invalid');
-        
+
     } else {
         inputRef.classList.add('invalid');
         inputRef.classList.remove('valid');
