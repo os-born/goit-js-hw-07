4354df97dbef6bfe691fa5e284a800d3671a824e
os-born <ol.serduk@gmail.com>
2021-07-12 17:10:10 +0300

    Task4Done

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+
+let counterValue = 0;
+
+const incrementRef = document.querySelector('[data-action="increment"]');
+const decrementRef = document.querySelector('[data-action="decrement"]');
+const numberOfCounterRef = document.querySelector('#value');
+
+const onClickIncrement = function () {
+    counterValue += 1;
+    numberOfCounter.textContent = counterValue;
+}
+const onClickDecrement = function() {
+    counterValue -= 1;
+    newValue.textContent = counterValue;
+}
+increment.addEventListener('click', onClickIncrement);
+decrement.addEventListener('click', onClickDecrement);
