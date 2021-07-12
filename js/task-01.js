d2be253a92359dbe420190c42d2e79716059c112
os-born <ol.serduk@gmail.com>
2021-07-12 18:38:10 +0300

    1Task_7Module_Done

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,12 @@
 const categories = document.querySelectorAll('li.item');
 console.log(`В списке ${categories.length} категории`);
+
 const arr = categories.forEach(category => {
-       
         console.log('Категория: ', category.firstElementChild.textContent);
-        const quantityOfCategoryList = category.firstElementChild.nextElementSibling.querySelectorAll('li').length;
+
+        const quantityOfCategoryList = 
+        category.firstElementChild.nextElementSibling.querySelectorAll('li').length;
+
         return console.log(`Количество элементов: ${quantityOfCategoryList}`);
     }
 )
